e552956415a7dc2d59cd9210d3849d90bb0f94be
mango12q <2998813400@qq.com>
2026-09-22 22:52:01 +0800

    chore: remove obsolete buggy scripts, duplicate images and stale files

--- results/_obsolete_buggy/check_eobs_dims2.R
+++
@@ -1,8 +0,0 @@
-library(ncdf4)
-
-nc <- nc_open('E:/2607compound/data/E-OBS/EOBS_tg_1983_2023.nc')
-cat('t2m dimensions:', dim(nc$var$T2m), '\n')
-cat('time length:', nc$dim$time$len, '\n')
-cat('lat length:', nc$dim$lat$len, '\n')
-cat('lon length:', nc$dim$lon$len, '\n')
-nc_close(nc)
--- results/_obsolete_buggy/check_eobs_vars.R
+++
@@ -1,9 +0,0 @@
-library(ncdf4)
-
-nc <- nc_open('E:/2607compound/data/E-OBS/EOBS_tg_1983_2023.nc')
-cat('Variables:', names(nc$var), '\n')
-cat('Dimensions:', names(nc$dim), '\n')
-for (d in names(nc$dim)) {
-  cat(' ', d, ':', nc$dim[[d]]$len, '\n')
-}
-nc_close(nc)
